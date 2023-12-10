01ab6f235802d401b31fd9bc5480b48ce1e1bc90
Allen B. Taylor <a.b.taylor@gmail.com>
2023-12-10 17:33:24 -0500

    Added more NaN tests.

--- scripts/tests/nan.asp
+++ scripts/tests/nan.asp
@@ -1,3 +1,12 @@
+def comps(a, b):
+    print('%s == %s' % (a, b), a == b)
+    print('%s != %s' % (a, b), a != b)
+    print('%s < %s' % (a, b), a < b)
+    print('%s <= %s' % (a, b), a <= b)
+    print('%s > %s' % (a, b), a > b)
+    print('%s >= %s' % (a, b), a >= b)
+    print('---')
+
 print((-1) ** 0.5)
 print(acos(-1.1))
 print('---')
@@ -8,10 +17,16 @@ pi = acos(-1)
 nums = (nnan, pnan, pi)
 for i in nums:
     for j in nums:
-        print('%s == %s' % (i, j), i == j)
-        print('%s != %s' % (i, j), i != j)
-        print('%s < %s' % (i, j), i < j)
-        print('%s <= %s' % (i, j), i <= j)
-        print('%s > %s' % (i, j), i > j)
-        print('%s >= %s' % (i, j), i >= j)
-        print('---')
+        comps(i, j)
+
+a = (1, 2, 3)
+b = (1, 2, pnan)
+comps(a, b)
+
+a = (5, (2, 0), 3)
+b = (5, (2, pnan), 4)
+c = (5, (2, pnan), 4)
+comps(a, a)
+comps(a, b)
+comps(b, b) # Identical objects compare normally, even if containing NaNs.
+comps(b, c) # Different objects with same values compare according to contained NaNs.
--- scripts/tests/nan.py
+++ scripts/tests/nan.py
@@ -1,4 +1,16 @@
+#!/bin/env python3
+
 import math
+
+def comps(a, b):
+        print('%s == %s' % (a, b), a == b)
+        print('%s != %s' % (a, b), a != b)
+        print('%s < %s' % (a, b), a < b)
+        print('%s <= %s' % (a, b), a <= b)
+        print('%s > %s' % (a, b), a > b)
+        print('%s >= %s' % (a, b), a >= b)
+        print('---')
+
 print((-1) ** 0.5)
 print((-1) ** (-0.5))
 print('---')
@@ -9,10 +21,18 @@ pi = math.acos(-1)
 nums = (nnan, pnan, pi)
 for i in nums:
     for j in nums:
-        print('%s == %s' % (i, j), i == j)
-        print('%s != %s' % (i, j), i != j)
-        print('%s < %s' % (i, j), i < j)
-        print('%s <= %s' % (i, j), i <= j)
-        print('%s > %s' % (i, j), i > j)
-        print('%s >= %s' % (i, j), i >= j)
-        print('---')
+        comps(i, j)
+
+a = (1, 2, 3)
+b = (1, 2, pnan)
+comps(a, b)
+
+a = (5, (2, 0), 3)
+b = (5, (2, pnan), 4)
+c = (5, (2, pnan), 4)
+comps(a, a)
+comps(a, b)
+comps(b, b) # Identical objects compare normally, even if containing NaNs.
+print(id(b),id(c))
+print(id(b[1]),id(c[1]))
+comps(b, c) # Python yiels unexpected normal comparison results, as if NaNs are not compared
