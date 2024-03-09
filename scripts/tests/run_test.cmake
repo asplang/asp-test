d115264d7512a8d9af53abc75abee83814b8c52c
Allen B. Taylor <a.b.taylor@gmail.com>
2024-03-08 21:08:03 -0500

    Split output from test scripts into three parts: script printing, trace, and dump.

--- scripts/tests/run_test.cmake
+++ scripts/tests/run_test.cmake
@@ -1,7 +1,7 @@
 execute_process(
-    COMMAND "${ASPS_EXECUTABLE}" "${BYTECODE_FILE}"
-    OUTPUT_FILE "${OUTPUT_DIR}/${TEST_NAME}.out"
-    ERROR_FILE "${OUTPUT_DIR}/${TEST_NAME}.out"
+    COMMAND "${ASPS_EXECUTABLE}" -T 2 -u "${OUTPUT_DIR}/${TEST_NAME}.dump.txt" "${BYTECODE_FILE}"
+    OUTPUT_FILE "${OUTPUT_DIR}/${TEST_NAME}.out.txt"
+    ERROR_FILE "${OUTPUT_DIR}/${TEST_NAME}.trace.txt"
     RESULT_VARIABLE run_result
     )
 
@@ -11,12 +11,20 @@ if (NOT ("${REFERENCE_DIR}" STREQUAL ""))
         set(extra_diff_opts --ignore-eol)
     endif()
 
-    execute_process(
-        COMMAND ${CMAKE_COMMAND} -E compare_files ${extra_diff_opts} "${REFERENCE_DIR}/${TEST_NAME}.out" "${OUTPUT_DIR}/${TEST_NAME}.out"
-        RESULT_VARIABLE diff_result
-        )
+    set(test_result 0)
+    foreach(suffix .out.txt .trace.txt .dump.txt)
+        execute_process(
+            COMMAND ${CMAKE_COMMAND} -E compare_files ${extra_diff_opts} "${REFERENCE_DIR}/${TEST_NAME}${suffix}" "${OUTPUT_DIR}/${TEST_NAME}${suffix}"
+            RESULT_VARIABLE diff_result
+            )
 
-    if (NOT (${diff_result} EQUAL 0))
-        message(FATAL_ERROR "${TEST_NAME}: Output \"${OUTPUT_DIR}/${TEST_NAME}.out\" differs from the expected output file \"${REFERENCE_DIR}/${TEST_NAME}.out\"")
+        if (NOT (${diff_result} EQUAL 0))
+            set(test_result 1)
+            message(NOTICE "${TEST_NAME}: Output \"${OUTPUT_DIR}/${TEST_NAME}${suffix}\" differs from the expected output file \"${REFERENCE_DIR}/${TEST_NAME}${suffix}\"")
+        endif()
+    endforeach()
+
+    if (NOT (${test_result} EQUAL 0))
+        message(FATAL_ERROR "${TEST_NAME}: Test failed")
     endif()
 endif()
