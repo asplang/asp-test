d4ecb305a2ed6896bfbe02c4e961956ff4d980bf
Lassi Hämäläinen <lassi.m.hamalainen@gmail.com>
2024-04-15 21:07:36 +0300

    Compile test scripts during the test and record compilation output
    - This allows regression testing of compile failures

--- scripts/tests/run_test.cmake
+++ scripts/tests/run_test.cmake
@@ -1,30 +1,52 @@
 execute_process(
-    COMMAND "${ASPS_EXECUTABLE}" -T 2 -u "${OUTPUT_DIR}/${TEST_NAME}.dump.txt" "${BYTECODE_FILE}"
-    OUTPUT_FILE "${OUTPUT_DIR}/${TEST_NAME}.out.txt"
-    ERROR_FILE "${OUTPUT_DIR}/${TEST_NAME}.trace.txt"
-    RESULT_VARIABLE run_result
-    )
+    COMMAND "${ASPC_EXECUTABLE}" -o "${BYTECODE_FILE}" "${ASPEC_FILE}" "${SOURCE_FILE}"
+    ERROR_FILE "${OUTPUT_DIR}/${TEST_NAME}.aspc.err.txt"
+    RESULT_VARIABLE compile_result
+)
+
+set(extra_diff_opts)
+if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.14")
+    set(extra_diff_opts --ignore-eol)
+endif()
 
 if (NOT ("${REFERENCE_DIR}" STREQUAL ""))
-    set(extra_diff_opts)
-    if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.14")
-        set(extra_diff_opts --ignore-eol)
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -E compare_files ${extra_diff_opts} "${REFERENCE_DIR}/${TEST_NAME}.aspc.err.txt" "${OUTPUT_DIR}/${TEST_NAME}.aspc.err.txt"
+        RESULT_VARIABLE diff_result
+    )
+
+    if (NOT (${diff_result} EQUAL 0))
+        message(FATAL_ERROR "${TEST_NAME}: aspc output differs from the expected output file \"${REFERENCE_DIR}/${TEST_NAME}.aspc.err.txt\"")
     endif()
+endif()
 
-    set(test_result 0)
-    foreach(suffix .out.txt .trace.txt .dump.txt)
-        execute_process(
-            COMMAND ${CMAKE_COMMAND} -E compare_files ${extra_diff_opts} "${REFERENCE_DIR}/${TEST_NAME}${suffix}" "${OUTPUT_DIR}/${TEST_NAME}${suffix}"
-            RESULT_VARIABLE diff_result
-            )
+if (${compile_result} EQUAL 0)
+    execute_process(
+        COMMAND "${ASPS_EXECUTABLE}" -T 2 -u "${OUTPUT_DIR}/${TEST_NAME}.dump.txt" "${BYTECODE_FILE}"
+        OUTPUT_FILE "${OUTPUT_DIR}/${TEST_NAME}.out.txt"
+        ERROR_FILE "${OUTPUT_DIR}/${TEST_NAME}.trace.txt"
+        RESULT_VARIABLE run_result
+        )
 
-        if (NOT (${diff_result} EQUAL 0))
-            set(test_result 1)
-            message(NOTICE "${TEST_NAME}: Output \"${OUTPUT_DIR}/${TEST_NAME}${suffix}\" differs from the expected output file \"${REFERENCE_DIR}/${TEST_NAME}${suffix}\"")
+    if (NOT ("${REFERENCE_DIR}" STREQUAL ""))
+        set(test_result 0)
+        foreach(suffix .out.txt .trace.txt .dump.txt)
+            execute_process(
+                COMMAND ${CMAKE_COMMAND} -E compare_files ${extra_diff_opts} "${REFERENCE_DIR}/${TEST_NAME}${suffix}" "${OUTPUT_DIR}/${TEST_NAME}${suffix}"
+                RESULT_VARIABLE diff_result
+                )
+
+            if (NOT (${diff_result} EQUAL 0))
+                set(test_result 1)
+                message(NOTICE "${TEST_NAME}: Output \"${OUTPUT_DIR}/${TEST_NAME}${suffix}\" differs from the expected output file \"${REFERENCE_DIR}/${TEST_NAME}${suffix}\"")
+            endif()
+        endforeach()
+
+        if (NOT (${test_result} EQUAL 0))
+            message(FATAL_ERROR "${TEST_NAME}: Test failed")
         endif()
-    endforeach()
-
-    if (NOT (${test_result} EQUAL 0))
-        message(FATAL_ERROR "${TEST_NAME}: Test failed")
     endif()
+else()
+    # Delete any old output files if compile fails
+    file(REMOVE "${OUTPUT_DIR}/${TEST_NAME}.dump.txt" "${OUTPUT_DIR}/${TEST_NAME}.out.txt" "${OUTPUT_DIR}/${TEST_NAME}.trace.txt")
 endif()
